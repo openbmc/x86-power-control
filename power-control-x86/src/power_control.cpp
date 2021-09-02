1ef0a1665d30f781562972ec23cbdb7bb84ac761
Zev Weiss <zev@bewilderbeest.net>
2021-09-01 22:36:03 -0500

    Fix powerControlDir creation failure error message
    It had had a ": " separator that got lost in a previous edit.
    
    Fixes: 41a49aa647a3c ("Use phosphor::logging instead of std::cerr")
    Change-Id: I5221a461a43333623ce1cfb7f7ea2b900ecb50a5
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>

--- power-control-x86/src/power_control.cpp
+++ power-control-x86/src/power_control.cpp
@@ -757,7 +757,7 @@ static int initializePowerStateStorage()
         if (ec.value() != 0)
         {
             std::string errMsg =
-                "failed to create " + powerControlDir.string() + ec.message();
+                "failed to create " + powerControlDir.string() + ": " + ec.message();
             phosphor::logging::log<phosphor::logging::level::ERR>(
                 errMsg.c_str());
             return -1;
