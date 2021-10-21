dfc901ba6e0824e9ffc05e207fd1421b9697e62d
Jason M. Bills <jason.m.bills@linux.intel.com>
2021-10-21 14:08:02 -0700

    Remove i2c support
    I2c is only used for direct SMBus commands to the PCH which are
    not really needed as the hardware power button override is
    always expected to work.
    
    Removing this command allows us to remove i2c support and greatly
    simplify the x86-power-control build.
    
    Tested:
    Confirmed that ipmi power control commands all still work correctly.
    
    Change-Id: I5fe48c4577efcb52530c5af699e9cdc26084e261
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- power-control-x86/src/power_control.cpp
+++ power-control-x86/src/power_control.cpp
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 */
-#include "i2c.hpp"
-
 #include <sys/sysinfo.h>
 #include <systemd/sd-journal.h>
 
@@ -1352,17 +1350,7 @@ static void forcePowerOff()
         }
 
         phosphor::logging::log<phosphor::logging::level::INFO>(
-            "PCH Power-button override failed. Issuing Unconditional Powerdown SMBus command.");
-        const static constexpr size_t pchDevBusAddress = 3;
-        const static constexpr size_t pchDevSlaveAddress = 0x44;
-        const static constexpr size_t pchCmdReg = 0;
-        const static constexpr size_t pchPowerDownCmd = 0x02;
-        if (i2cSet(pchDevBusAddress, pchDevSlaveAddress, pchCmdReg,
-                   pchPowerDownCmd) < 0)
-        {
-            phosphor::logging::log<phosphor::logging::level::ERR>(
-                "Unconditional Powerdown command failed! Not sure what to do now.");
-        }
+            "PCH Power-button override failed. Not sure what to do now.");
     });
 }
 
