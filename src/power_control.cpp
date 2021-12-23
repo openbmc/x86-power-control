f45dd176552c4478595e04653676def8339437e4
Tim Lee <timlee660101@gmail.com>
2021-12-23 11:46:01 +0800

    Correct osState string to match xyz naming format for fixing automation test
    Symptom:
    "Verify Dump After Host Watchdog Error Injection" automation test item failed.
    
    Root cause:
    Due to "Is OS Booted" keyword got failed in lib/state_manager.robot.
    
    According openbmc-test-automation/lib/variables.py
    The "os_state" must match to
    "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby".
    
    But, "osState" string in x86-power-control just return "Standby" instead of
    "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby".
    Thus keyword "Is OS Booted" return failed then cause test item got failed.
    
    Reference:
    openbmc-test-automation/lib/state_manager.robot:
    Is OS Booted
        [Documentation]  Check OS status.
    ${os_state}=  Get Host State Attribute  OperatingSystemState
    Should Be Equal  ${OS_BOOT_COMPLETE}  ${os_state}
    
    openbmc-test-automation/lib/variables.py:
    STATE_DBUS_BASE = 'xyz.openbmc_project.State.'
    OS_BOOT_COMPLETE = STATE_DBUS_BASE + \
        'OperatingSystem.Status.OSStatus.Standby'
    
    Solution:
    Correct osState string to match xyz naming format and consistent with auto test.
    
    Tested:
    Host Power ON:
    busctl call xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 \
    org.freedesktop.DBus.Properties \
    GetAll s "xyz.openbmc_project.State.OperatingSystem.Status"
    a{sv} 1 "OperatingSystemState" s "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby"
    
    Host Power OFF:
    busctl call xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 \
    org.freedesktop.DBus.Properties \
    GetAll s "xyz.openbmc_project.State.OperatingSystem.Status"
    "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive"
    
    Signed-off-by: Tim Lee <timlee660101@gmail.com>
    Change-Id: Ia63100a6756104a645c2d789d3b5be560a1305ff

--- src/power_control.cpp
+++ src/power_control.cpp
@@ -205,6 +205,36 @@ static void beep(const uint8_t& beepPriority)
         "xyz.openbmc_project.BeepCode", "Beep", uint8_t(beepPriority));
 }
 
+enum class OperatingSystemStateStage
+{
+    Inactive,
+    Standby,
+};
+static constexpr std::string_view
+    getOperatingSystemStateStage(const OperatingSystemStateStage stage)
+{
+    switch (stage)
+    {
+        case OperatingSystemStateStage::Inactive:
+            return "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive";
+            break;
+        case OperatingSystemStateStage::Standby:
+            return "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby";
+            break;
+        default:
+            return "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive";
+            break;
+    }
+};
+static void setOperatingSystemState(const OperatingSystemStateStage stage)
+{
+    osIface->set_property("OperatingSystemState",
+                          std::string(getOperatingSystemStateStage(stage)));
+
+    lg2::info("Moving os state to {STATE} stage",
+              "STATE", std::string(getOperatingSystemStateStage(stage)));
+}
+
 enum class PowerState
 {
     on,
@@ -1480,6 +1510,9 @@ static void currentHostStateMonitor()
     else
     {
         pohCounterTimer.cancel();
+        // If HostState is trurned to OFF, Set 'OperatingSystemState' to
+        // 'Inactive'
+        setOperatingSystemState(OperatingSystemStateStage::Inactive);
         // Set the restart cause set for this restart
         setRestartCause();
     }
@@ -1541,8 +1574,7 @@ static void currentHostStateMonitor()
                 // 'OperatingSystemState' to stay at 'Standby', even though
                 // system is OFF. Set 'OperatingSystemState' to 'Inactive'
                 // if HostState is trurned to OFF.
-                osIface->set_property("OperatingSystemState",
-                                      std::string("Inactive"));
+                setOperatingSystemState(OperatingSystemStateStage::Inactive);
 
                 // Set the restart cause set for this restart
                 setRestartCause();
@@ -2158,12 +2190,12 @@ static void postCompleteHandler(bool state)
     if (!state)
     {
         sendPowerControlEvent(Event::postCompleteAssert);
-        osIface->set_property("OperatingSystemState", std::string("Standby"));
+        setOperatingSystemState(OperatingSystemStateStage::Standby);
     }
     else
     {
         sendPowerControlEvent(Event::postCompleteDeAssert);
-        osIface->set_property("OperatingSystemState", std::string("Inactive"));
+        setOperatingSystemState(OperatingSystemStateStage::Inactive);
     }
 }
 
@@ -3191,17 +3223,23 @@ int main(int argc, char* argv[])
     // Get the initial OS state based on POST complete
     //      0: Asserted, OS state is "Standby" (ready to boot)
     //      1: De-Asserted, OS state is "Inactive"
-    std::string osState;
+    static OperatingSystemStateStage osState;
     if (postCompleteConfig.type == ConfigType::GPIO)
     {
-        osState = postCompleteLine.get_value() > 0 ? "Inactive" : "Standby";
+        osState = postCompleteLine.get_value() > 0
+                      ? OperatingSystemStateStage::Inactive
+                      : OperatingSystemStateStage::Standby;
     }
     else
     {
-        osState = getProperty(postCompleteConfig) > 0 ? "Inactive" : "Standby";
+        osState = getProperty(postCompleteConfig) > 0
+                      ? OperatingSystemStateStage::Inactive
+                      : OperatingSystemStateStage::Standby;
     }
 
-    osIface->register_property("OperatingSystemState", std::string(osState));
+    osIface->register_property(
+        "OperatingSystemState",
+        std::string(getOperatingSystemStateStage(osState)));
 
     osIface->initialize();
 
